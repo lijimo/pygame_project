13abfddf07c624ab05321d92c6c2c25da4e5ae40
lijimo <36312082+lijimo@users.noreply.github.com>
2024-04-04 00:50:05 +0200

    display all animals tiles on screen

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import pygame
-from pygame import display, event, image
+import game_config as gc
+from pygame import display, event, image, transform
+from animal import Animal
 
 pygame.init()
 
 display.set_caption('My Memory Game')
 screen = display.set_mode((512, 512))
-running = True
+
 matched = image.load('other_assets/matched.png')
-# display the matched symbol on the screen
-screen.blit(matched, (0,0))
-display.flip()
+matched = transform.scale(matched, (512, 512))
+
+running = True
+tiles = [Animal(i) for i in range(0, gc.NUM_TILES_TOTAL)]
 
 while running:
     # list of all keyboard and mouse events
@@ -18,5 +21,12 @@ while running:
         # stop game if users quits
         if e.type == pygame.QUIT:
             running = False
+    # initial blank screen
+    screen.fill((255,255,255))
+    # display animals
+    for tile in tiles:
+        screen.blit(tile.image, (tile.col * gc.IMAGE_SIZE, tile.row * gc.IMAGE_SIZE))
+    display.flip()
+
 
 print("Bye, bye!") 

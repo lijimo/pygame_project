5048c05846fd25142827b37ca3131c3284978bf2
lijimo <36312082+lijimo@users.noreply.github.com>
2024-04-04 01:56:25 +0200

    fixed bug in ASSET_FILES

--- game_config.py
+++ game_config.py
@@ -8,8 +8,9 @@ MARGIN = 4
 
 ASSET_DIR = 'assets'
 print('directory:')
-print(os.listdir(ASSET_DIR))
-ASSET_FILES = [x for x in os.listdir(ASSET_DIR) if x[-3].lower() == 'png'] 
+ASSET_FILES = [x for x in os.listdir(ASSET_DIR) if x[-3:].lower() == 'png'] 
+print(ASSET_FILES)
 # check if there are all required tiles
+print(len(ASSET_FILES))
 assert len(ASSET_FILES) == 8
 
